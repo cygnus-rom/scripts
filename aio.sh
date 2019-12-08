cc035cd78f7a090d4a212acf6f3432ce126276fb
Dhruv Gera <42107491+Dhruvgera@users.noreply.github.com>
2019-12-08 16:03:05 +0530

    Create aio.sh

---
+++ aio.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+cd $HOME
+mkdir cygnus
+cd cygnus
+repo init -u git://github.com/cygnus-rom/manifest.git -b default
+repo sync -j$(nproc --all) --force-sync --no-tags --no-clone-bundle --prune --optimized-fetch
+git clone https://github.com/cygnus-rom/scripts.git
+mv scripts/updates.sh $PWD
+bash updates.sh
+.b*/e*
