423cd52aa31d37910d68320391187182f09e2718
dhruv <dhruvgera61@gmail.com>
2019-12-30 19:21:58 +0530

    Remove un-needed code
    Signed-off-by: Dhruvgera <dhruvgera61@gmail.com>

--- aio.sh
+++ aio.sh
@@ -2,9 +2,9 @@
 cd $HOME
 mkdir cygnus
 cd cygnus
+rm -rf  hardware/qcom-caf/msm8996/audio lineage-sdk/ packages/apps/ExactCalculator packages/apps/LineageParts/ packages/apps/Nfc/ packages/apps/Settings/ packages/apps/SetupWizard/ packages/apps/Trebuchet/ system/netd/ system/vold/ build/make/ 
 repo init -u git://github.com/cygnus-rom/manifest.git -b default
 repo sync -j$(nproc --all) --force-sync --no-tags --no-clone-bundle --prune --optimized-fetch
-git clone https://github.com/cygnus-rom/scripts.git
 mv $HOME/scripts/updates.sh $PWD
 bash updates.sh
 
