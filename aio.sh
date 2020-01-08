e854cbb7877c626bcd369ab00624f2b84eadd6a5
Dhruv Gera <42107491+Dhruvgera@users.noreply.github.com>
2020-01-08 22:42:08 +0530

    Update aio.sh

--- aio.sh
+++ aio.sh
@@ -3,7 +3,7 @@ cd $HOME
 mkdir cygnus
 cd cygnus
 rm -rf  hardware/qcom-caf/msm8996/audio lineage-sdk/ packages/apps/ExactCalculator packages/apps/LineageParts/ packages/apps/Nfc/ packages/apps/Settings/ packages/apps/SetupWizard/ packages/apps/Trebuchet/ system/netd/ system/vold/ build/make/ 
-repo init -u git://github.com/cygnus-rom/manifest.git -b default
+repo init -u git://github.com/cygnus-rom/manifest.git -b ten-rebase
 repo sync -j$(nproc --all) --force-sync --no-tags --no-clone-bundle --prune --optimized-fetch
 mv $HOME/scripts/updates.sh $PWD
 bash updates.sh
