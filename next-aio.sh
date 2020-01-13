224aa85888628a862203a8527ad4b5b60a26c186
Dhruv Gera <42107491+Dhruvgera@users.noreply.github.com>
2020-01-13 17:48:00 +0530

    Create next-aio.sh
    Made for our testing org

---
+++ next-aio.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+cd $HOME
+mkdir cygnus
+cd cygnus
+rm -rf  hardware/qcom-caf/msm8996/audio lineage-sdk/ packages/apps/ExactCalculator packages/apps/LineageParts/ packages/apps/Nfc/ packages/apps/Settings/ packages/apps/SetupWizard/ packages/apps/Trebuchet/ system/netd/ system/vold/ build/make/
+repo init -u git://github.com/cygnus-next/manifest.git 
+repo sync -j$(nproc --all) --force-sync --no-tags --no-clone-bundle --prune --optimized-fetch
+mv $HOME/scripts/updates.sh $PWD
+bash updates.sh
