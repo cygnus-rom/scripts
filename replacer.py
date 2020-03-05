d85d672292ebb28b2284e10ce5116f0539ccf056
Dhruv Gera <42107491+Dhruvgera@users.noreply.github.com>
2020-03-05 20:20:02 +0530

    CI setup [2/2]

---
+++ replacer.py
@@ -0,0 +1,15 @@
+import os
+import time
+import glob
+import subprocess
+import select
+
+fin = open("uploader.py", "rt")
+data = fin.read()
+data = data.replace('q', devicename)
+fin.close()
+
+fin = open("uploader.py", "wt")
+fin.write(data)
+fin.close()
+os.system("python3 uploader.py")
