b3c36f095753a03b9143289bcbfb0622726bc72c
Dhruv Gera <42107491+Dhruvgera@users.noreply.github.com>
2020-03-05 20:17:20 +0530

    CI setup [1/2]

---
+++ uploader.py
@@ -0,0 +1,36 @@
+# Copyright (C) Dhruv Gera
+import time
+import subprocess
+import select
+
+listdevice=[device]
+
+import os, glob
+objects = os.listdir("$PWD")
+sofar = 0
+name = ""
+for item in objects:
+        size = os.path.getsize(item)
+        if size > sofar:
+                sofar = size
+                name = ""+item
+os.environ["name"] = largest
+import subprocess
+import select
+cmd = subprocess.Popen(['bash'], stdin=subprocess.PIPE, stdout=subprocess.PIPE)
+os.system("$PWD/upload.sh")
+
+
+objects = os.listdir("out/target/product/q/obj/PACKAGING/target_files_intermediates")
+sofar = 0
+name = ""
+for item in objects:
+        size = os.path.getsize(item)
+        if size > sofar:
+                sofar = size
+                name = "out/target/product/q/obj/PACKAGING/target_files_intermediates"+item
+os.environ["name"] = largest
+import subprocess
+import select
+cmd = subprocess.Popen(['bash'], stdin=subprocess.PIPE, stdout=subprocess.PIPE)
+os.system("$PWD/upload.sh")
