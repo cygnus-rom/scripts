2e6c684c520a58eacc2185ed35708851d29d43aa
dhruv <dhruvgera61@gmail.com>
2020-03-09 14:18:31 +0100

    upload: refactor and clean code

--- upload.sh
+++ upload.sh
@@ -1,8 +1,6 @@
 export ZIPNAME="log.txt"
 export CHAT_ID=""
 export BOT_API_KEY=""
-curl https://bashupload.com/$largest --data-binary @$largest |
-  while IFS= read -r line
-  do
-    curl -F chat_id="$CHAT_ID" -F document=@"$ZIPNAME" -F caption="$line" https://api.telegram.org/bot$BOT_API_KEY/sendDocument
-  done
+x=$(curl https://bashupload.com/$largest --data-binary @$largest)
+echo "$x" 
+curl -F chat_id="$CHAT_ID" -F document=@"$ZIPNAME" -F caption="$x" https://api.telegram.org/bot$BOT_API_KEY/sendDocument
